107cafb234d3dfb38ee0600ed38488c777bbc06a
nrsmsf <nrsmsf@ya.ru>
2019-04-13 21:58:32 +0300

    Initial Commit

---
+++ AutoLayout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout Calculator
+//
+//  Created by Andrey Nersesyan on 13/04/2019.
+//  Copyright © 2019 Andrey Nersesyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
